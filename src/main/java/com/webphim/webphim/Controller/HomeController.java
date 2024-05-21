8744799e7d2854dd8f3dd54eae58ecfc64aa5078
phungduydat <duydatphung7@gmail.com>
2024-05-21 11:34:32 +0700

    1.0

--- src/main/java/com/webphim/webphim/Controller/HomeController.java
+++ src/main/java/com/webphim/webphim/Controller/HomeController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.webphim.webphim;
+package com.webphim.webphim.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author Acer
  */
 @Controller
-@RequestMapping("/Movies")
-public class MoviesController {
+@RequestMapping("/Home")
+public class HomeController { 
+    
     @GetMapping("")
-    public String movies() {
-        return "Movies/movies";
+    public String index(){
+        return "Home/index";
     }
-    @GetMapping("/movie-details")
-    public String movie_details() {
-        return "Movies/movie-details";
-    }
-    @GetMapping("/top-movies")
-    public String top_movies() {
-        return "Movies/top-movies";
-    }
+    
 }
